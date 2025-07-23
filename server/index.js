5f61f2fa9f9e1a18543149f3c3430a5192f17f47
BoonruksaBL036 <664259036@webmail.npru.ac.th>
2025-07-24 01:29:05 +0700

    authentication

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ import dotenv from "dotenv";
 dotenv.config()
 const PORT = process.env.PORT || 5000;
 import restaurantRouter from "./routers/restaurant.router.js"
+import authRouter from "./routers/auth.router.js";
+import db from "./models/index.js"
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("create table user_roles");
+});
+
 import cors from "cors"
 app.use(cors({
   origin: ["http://localhost:5173","127.0.0.1:5173"],
@@ -20,6 +26,9 @@ app.get("/", (req, res) => {
 
 //use router
 app.use("/api/v1/restaurants",restaurantRouter);
+
+//use authentication router
+app.use("/api/v1", authRouter);
 
 app.listen(PORT,()=>{
     console.log("Listening to http://localhost:" + PORT);
