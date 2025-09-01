839ae8d5e864ae213e4b149a056ccc21b7b3fc8e
BoonruksaBL036 <664259036@webmail.npru.ac.th>
2025-09-01 15:53:02 +0700

    deploy front

--- server/routers/restaurant.router.js
+++ server/routers/restaurant.router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const { verifyToken, isAdmin, isModOrAdmin } = authMiddleware;
 
 //POST http://localhost:5000/api/v1/restaurants
-router.post("/", verifyToken ,isAdmin, isModOrAdmin, restaurantController.create);
+router.post("/", verifyToken, isModOrAdmin, restaurantController.create);
 
 //GET http://localhost:5000/api/v1/restaurants
 router.get("/", verifyToken, restaurantController.getAll);
@@ -15,9 +15,9 @@ router.get("/", verifyToken, restaurantController.getAll);
 router.get("/:id", verifyToken, restaurantController.getById);
 
 //PUT http://localhost:5000/api/v1/restaurants/id
-router.put("/:id", verifyToken, isAdmin, isModOrAdmin, restaurantController.update);
+router.put("/:id", verifyToken, isModOrAdmin, restaurantController.update);
 
 //DELETE http://localhost:5000/api/v1/restaurants/id
-router.delete("/:id", verifyToken,isAdmin, restaurantController.deleteById);
+router.delete("/:id", verifyToken, isAdmin, restaurantController.deleteById);
 
 export default router;
